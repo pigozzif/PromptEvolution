a27b7c8d52fa58e08a44d54d96e352a56c34045d
pigozzif <pigozzife@gmail.com>
2023-10-06 19:53:58 +0200

    prompt factory

---
+++ prompt_factory.py
@@ -0,0 +1,28 @@
+from data.instruction_induction.load_data import load_data
+
+
+class PromptFactory(object):
+    DEMO = "I instruct my friend to <mask>\nThe friend read the instruction and wrote an output for every one of " \
+           "the inputs. Here are the input-output pairs:"
+    DEMO_FORWARD = "I gave a friend an instruction and five inputs. The friend read the instruction and wrote an " \
+                   "output for every one of the inputs. Here are the input-output pairs:\n[EXAMPLES]\nThe instruction" \
+                   " was "
+
+    def __init__(self, tokenizer, model, sub_task):
+        self.tokenizer = tokenizer
+        self.model = model
+        self.test_data = load_data("eval", sub_task)
+        self._fill_demo()
+
+    def _fill_demo(self):
+        self.DEMO = "\n".join([self.DEMO] + ["Input {0} Output {1}".format(i, o[0])
+                                             for i, o in zip(self.test_data[0], self.test_data[1])]).lower()
+        self.DEMO_FORWARD = self.DEMO_FORWARD.replace("[EXAMPLES]", "\n".join(["Input {0} Output {1}".format(i, o[0])
+                                                                               for i, o in zip(self.test_data[0],
+                                                                                               self.test_data[
+                                                                                                   1])]))
+
+    def create(self):
+        inputs = self.tokenizer(self.DEMO_FORWARD, truncation=True, max_length=1024, return_tensors="pt")
+        outputs = self.model.generate(**inputs, max_new_tokens=20 + len(inputs.input_ids))
+        return self.tokenizer.batch_decode(outputs, skip_special_tokens=True)[0][len(self.DEMO_FORWARD):]
