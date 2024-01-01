0deb1616633fdae5baf574da62fe518d23f70c17
pigozzif <pigozzife@gmail.com>
2024-01-01 19:07:06 +0100

    plot VAE results and better inference

--- Sentence-VAE/inference.py
+++ Sentence-VAE/inference.py
@@ -8,17 +8,15 @@ from utils import to_var, idx2word, interpolate
 
 
 def main(args):
-    with open(args.data_dir + '/ptb.vocab.json', 'r') as file:
-        vocab = json.load(file)
-
-    w2i, i2w = vocab['w2i'], vocab['i2w']
-
-    model = SentenceVAE(
-        vocab_size=len(w2i),
-        sos_idx=w2i['<sos>'],
-        eos_idx=w2i['<eos>'],
-        pad_idx=w2i['<pad>'],
-        unk_idx=w2i['<unk>'],
+    datasets = OrderedDict()
+    for split in splits:
+        datasets[split] = create_dataset(args=args, split=split)
+    params = dict(
+        vocab_size=datasets["train"].vocab_size(),
+        sos_idx=datasets["train"].sos_idx(),
+        eos_idx=datasets["train"].eos_idx(),
+        pad_idx=datasets["train"].pad_idx(),
+        unk_idx=datasets["train"].unk_idx(),
         max_sequence_length=args.max_sequence_length,
         embedding_size=args.embedding_size,
         rnn_type=args.rnn_type,
@@ -29,12 +27,13 @@ def main(args):
         num_layers=args.num_layers,
         bidirectional=args.bidirectional
     )
+    model = SentenceVAE(**params)
 
-    if not os.path.exists(args.load_checkpoint):
-        raise FileNotFoundError(args.load_checkpoint)
+    if not os.path.exists(args.save_model_path):
+        raise FileNotFoundError(args.save_model_path)
 
-    model.load_state_dict(torch.load(args.load_checkpoint))
-    print("Model loaded from %s" % args.load_checkpoint)
+    model.load_state_dict(torch.load(args.save_model_path))
+    print("Model loaded from %s" % args.save_model_path)
 
     if torch.cuda.is_available():
         model = model.cuda()
@@ -42,39 +41,51 @@ def main(args):
     model.eval()
 
     samples, z = model.inference(n=args.num_samples)
-    print('----------SAMPLES----------')
-    print(*idx2word(samples, i2w=i2w, pad_idx=w2i['<pad>']), sep='\n')
+    print("----------SAMPLES----------")
+    print(*idx2word(samples, i2w=i2w, pad_idx=datasets["train"].pad_idx()), sep="\n")
 
     z1 = torch.randn([args.latent_size]).numpy()
     z2 = torch.randn([args.latent_size]).numpy()
     z = to_var(torch.from_numpy(interpolate(start=z1, end=z2, steps=8)).float())
     samples, _ = model.inference(z=z)
-    print('-------INTERPOLATION-------')
-    print(*idx2word(samples, i2w=i2w, pad_idx=w2i['<pad>']), sep='\n')
+    print("-------INTERPOLATION-------")
+    print(*idx2word(samples, i2w=i2w, pad_idx=datasets["train"].pad_idx()), sep='\n')
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-c', '--load_checkpoint', type=str)
-    parser.add_argument('-n', '--num_samples', type=int, default=10)
+    parser.add_argument("-n", "--num_samples", type=int, default=10)
 
-    parser.add_argument('-dd', '--data_dir', type=str, default='data')
-    parser.add_argument('-ms', '--max_sequence_length', type=int, default=50)
-    parser.add_argument('-eb', '--embedding_size', type=int, default=300)
-    parser.add_argument('-rnn', '--rnn_type', type=str, default='gru')
-    parser.add_argument('-hs', '--hidden_size', type=int, default=256)
-    parser.add_argument('-wd', '--word_dropout', type=float, default=0)
-    parser.add_argument('-ed', '--embedding_dropout', type=float, default=0.5)
-    parser.add_argument('-ls', '--latent_size', type=int, default=16)
-    parser.add_argument('-nl', '--num_layers', type=int, default=1)
-    parser.add_argument('-bi', '--bidirectional', action='store_true')
+    parser.add_argument("--dataset", type=str, default="minipile")
+    parser.add_argument("--s", type=int, default=0)
+    parser.add_argument('--create_data', action='store_true')
+    parser.add_argument("--max_sequence_length", type=int, default=64)
+    parser.add_argument("--test", action="store_true")
 
-    args = parser.parse_args()
+    parser.add_argument("-ep", "--epochs", type=int, default=10)
+    parser.add_argument("-bs", "--batch_size", type=int, default=32)
+    parser.add_argument("-lr", "--learning_rate", type=float, default=0.001)
 
-    args.rnn_type = args.rnn_type.lower()
+    parser.add_argument("-eb", "--embedding_size", type=int, default=300)
+    parser.add_argument("-rnn", "--rnn_type", type=str, default="gru")
+    parser.add_argument("-hs", "--hidden_size", type=int, default=256)
+    parser.add_argument("-nl", "--num_layers", type=int, default=1)
+    parser.add_argument("-bi", "--bidirectional", action="store_true")
+    parser.add_argument("-ls", "--latent_size", type=int, default=128)
+    parser.add_argument("-wd", "--word_dropout", type=float, default=0)
+    parser.add_argument("-ed", "--embedding_dropout", type=float, default=0.5)
 
-    assert args.rnn_type in ['rnn', 'lstm', 'gru']
-    assert 0 <= args.word_dropout <= 1
+    parser.add_argument("-af", "--anneal_function", type=str, default="logistic")
+    parser.add_argument("-k", "--k", type=float, default=0.0025)
+    parser.add_argument("-x0", "--x0", type=int, default=2500)
 
-    main(args)
+    parser.add_argument("-ld", "--log_dir", type=str, default="output")
+    parser.add_argument("-bin", "--save_model_path", type=str, default="models")
+
+    arguments = parser.parse_args()
+    assert arguments.rnn_type in ["rnn", "lstm", "gru"]
+    assert 0 <= arguments.word_dropout <= 1
+
+    main(arguments)
--- Sentence-VAE/plot_results.py
+++ Sentence-VAE/plot_results.py
@@ -7,18 +7,13 @@ import os
 def read_files(path):
     data = None
     for file in os.listdir(path):
-        if not file.endswith("csv") or "summary" in file:
+        if not file.endswith("txt") or "minipile" not in file:
             continue
-        d = pd.read_csv(os.path.join(path, file), sep=",")
-        d["approach"] = file.split(".")[0]
-        d["problem"] = file.split(".")[1]
-        d["seed"] = int(file.split(".")[2])
-        d = d[d["iter_idx"] != "iter_idx"]
-        d["iter_idx"] = d.apply(lambda row: int(row["iter_idx"]), axis=1)
-        for var in ["iter_best_fitness", "iter_eval_time", "iter_model_time"]:
-            d[var] = d[var].astype(np.float64)
-        d["iter_total_time"] = d["iter_eval_time"] + d["iter_model_time"]
-        # d["iter_total_time"] = d["iter_total_time"].cumsum()
+        d = pd.read_csv(os.path.join(path, file), sep=";")
+        d["seed"] = int(file.split(".")[1])
+        d["elbo.mean"] = d.apply(lambda row: float(row["elbo.mean"].split(",")[0].split("(")[1]), axis=1)
+        for col in ["batch.loss", "batch.nll.loss", "batch.kl.loss", "batch.kl.weight", "elbo.mean"]:
+            d[col] = d[col].astype(np.float64)
         if data is None:
             data = pd.DataFrame(columns=d.columns)
         data = pd.concat([data, d])
@@ -26,22 +21,21 @@ def read_files(path):
 
 
 def plot_vars(data, vs):
-    fig, axes = plt.subplots(figsize=(20, 20), nrows=len(vs), ncols=len(data["problem"].unique()))
-    for row, var in enumerate(vs):
-        for col, (problem, traj) in enumerate(data.groupby(["problem"])):
-            for approach, inner_traj in traj.groupby(["approach"]):
-                median = inner_traj.groupby(inner_traj.iter_idx)[var].median()
-                axes[row][col].plot(median, label=approach)
-                err = inner_traj.groupby(inner_traj.iter_idx)[var].std()
-                axes[row][col].fill_between(np.arange(len(median)), median - err, median + err, alpha=0.25)
-            axes[row][col].set_title(problem, fontsize=15)
-            axes[row][col].set_xlabel("iters", fontsize=15)
-            axes[row][col].legend()
-            axes[row][col].set_ylabel(var, fontsize=15)
+    fig, axes = plt.subplots(figsize=(20, 5), nrows=1, ncols=len(vs))
+    for col, var in enumerate(vs):
+        for split, traj in data.groupby(["split"]):
+            median = traj.groupby(traj.epoch)[var].median()
+            axes[col].plot(median, label=split)
+            err = traj.groupby(traj.epoch)[var].std()
+            axes[col].fill_between(np.arange(len(median)), median - err, median + err, alpha=0.25)
+        # axes[row][col].set_title(split, fontsize=15)
+        axes[col].set_xlabel("epochs", fontsize=15)
+        axes[col].legend()
+        axes[col].set_ylabel(var, fontsize=15)
 
 
 if __name__ == "__main__":
-    data = read_files("results")
-    plot_vars(data, ["iter_best_fitness", "iter_eval_time", "iter_model_time", "iter_total_time"])
+    data = read_files(os.path.join(os.getcwd(), "output"))
+    plot_vars(data, ["batch.loss", "batch.nll.loss", "batch.kl.loss", "elbo.mean"])
     plt.savefig("results.png")
     plt.clf()
